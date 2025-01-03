7a1cf0996fc9085d4d0de7b2631986389f4f78c3
Caleb Dodson <caleb.v.dodson@gmail.com>
2025-01-03 16:41:13 -0600

    CSS and styling on navbar and home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,18 +3,29 @@ import { Link, useNavigate } from 'react-router-dom'
 
 import '../App.css'
 import React from 'react'
-
-
+import userIcon from '../assets/UserIcon.png';
+import logoIcon from '../assets/RoundTable.png'
 
 function Navbar() {
 
   return (
     <nav className="navbar">
-        <h1>Navbar</h1>
-        <Link to='/'>Recipe Round Table</Link>
-        <input type='text'  placeholder='Search for recipes or users'></input>
-        <Link to='/user'>User</Link>
-        
+        <Link to='/'>
+          <div id="logoContainer">
+            <img id="logoIcon" src={logoIcon} alt="Website logo" />
+            <span id="logoText">Recipe Round Table</span>
+          </div>
+        </Link>
+        <div id="searchbarContainer">
+          <input id="searchbar" type='text'  placeholder='Search for recipes or users'></input>
+          <button>Search</button>
+        </div>
+        <Link id="userIcon" to='/user'>
+          <div id="userIconContainer">
+            <img src={userIcon} alt="User icon" />
+            <span>Login</span>
+          </div>
+        </Link>   
     </nav>
   )
 }
