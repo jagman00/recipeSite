aed0c1b1ba1e6e2fa0e2c91808caf06e0964ea79
Williamd110 <williamd.howard6@gmail.com>
2025-01-23 12:06:17 -0500

    loading push recipes.jsx

--- client/src/components/Bookmarks.jsx
+++ client/src/components/Bookmarks.jsx
@@ -49,8 +49,6 @@ const Bookmarks = () => {
     }
   }, [userId, token, page, bookmarkedRecipes.length]);
 
-  if (loading) return <div>Loading...</div>;
-
   // Handle pagination
   const handleNextPage = () => {
     if (page < totalPages) {
